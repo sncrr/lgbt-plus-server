23ebae4f19dfda1fd73292141dae39ef9f7335ab
Emerson Dalwampo <78949883+sncrr@users.noreply.github.com>
2022-10-05 20:31:18 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,19 @@
 module.exports = config = {
-  db: {
-    host: 'localhost',
-    port: 3306,
-    database: 'lgbt_plus_master',
-    user: 'root',
-    password: '',
-  },
   // db: {
-  //   host: '194.163.35.34',
+  //   host: 'localhost',
   //   port: 3306,
-  //   database: 'u135501428_lgbt_plus',
-  //   user: 'u135501428_lgbt_plus',
-  //   password: 'LgbtPlus2022'
+  //   database: 'lgbt_plus_master',
+  //   user: 'root',
+  //   password: '',
   // },
+  db: {
+    host: '194.163.35.34',
+    port: 3306,
+    database: 'u135501428_lgbt_plus',
+    user: 'u135501428_lgbt_plus',
+    password: 'LgbtPlus2022'
+  },
   server: {
-    url: 'http://localhost',
-    port: 8080,
+    url: 'https://lgbt-plus-server.herokuapp.com'
   }
 }

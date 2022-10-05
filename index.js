4bc902418c419dcd508afa956a18d721ba3ed56e
Emerson Dalwampo <78949883+sncrr@users.noreply.github.com>
2022-10-05 22:06:59 +0800

    Update index.js

--- index.js
+++ index.js
@@ -8,13 +8,21 @@ const cors = require('cors');
  * Express Settigns
  */
 const app = express();
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
 app.use(express.static('build'));
 app.use(express.urlencoded({
   extended: true
 }));
-app.options('*', cors());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  next();
+});
+
+// app.options('*', cors());
 
 /**
  * Initialize API

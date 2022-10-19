4130a35d9bc50b7a515bae4dbe8cbffcf2a32ff3
Emerson Dalwampo <78949883+sncrr@users.noreply.github.com>
2022-10-20 00:08:14 +0800

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, application/json");
+  res.header("Access-Control-Allow-Headers", "*");
   next();
 });
 
